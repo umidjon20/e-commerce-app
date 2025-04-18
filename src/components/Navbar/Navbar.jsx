c5ee51610f13208e959e9ee698583597205e9968
Umidjon Amirqulov <uamirqulov5@gmail.com>
2025-04-18 20:03:17 +0500

    install react-router-dom

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
-
-import './Navbar.scss'
+import {
+   Link
+  } from "react-router-dom";
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+    <header className='header'>
+        <div className="container d_flex">
+            <div className="categories d_flex">
+                <span className='fa-solid fa-border-all'></span>
+                <h4>Categories <i className='fa fa-chevron-down'></i></h4>
+            </div>
+        </div>
+        <div className="navlink">
+            <ul className='nav'>
+                <li>
+                    <Link to={'/'} >
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/pages'>
+                        Pages
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/user'>
+                        User account
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/vendor'>
+                        Vendor account
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/track'>
+                        Track my order
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/contact'>
+                        Contact
+                    </Link>
+                </li>
+            </ul>
+           
+        </div>
+    </header>
+    </>
   )
 }
